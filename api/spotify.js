e4961866d358cd9bb58423b04948c5ccb5dfabb8
David Vaz <74098173+davidvaz1@users.noreply.github.com>
2026-03-02 22:47:32 +0530

    Refactor Spotify and YouTube music fetching logic

--- api/spotify.js
+++ api/spotify.js
@@ -1,80 +1,26 @@
 module.exports = (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     
-    // LOG EVERYTHING
-    const debug = {
-        timestamp: new Date().toISOString(),
-        spotify_env: !!(process.env.SPOTIFY_CLIENT_ID && process.env.SPOTIFY_CLIENT_SECRET),
-        client_id_exists: !!process.env.SPOTIFY_CLIENT_ID,
-        client_secret_exists: !!process.env.SPOTIFY_CLIENT_SECRET
-    };
-    
-    if (debug.spotify_env) {
-        debug.attempting = 'Spotify';
-        trySpotify();
-    } else {
-        debug.attempting = 'YouTube';
-        tryYouTube();
-    }
-    
-    function trySpotify() {
-        const auth = Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64');
-        
-        fetch('https://accounts.spotify.com/api/token', {
-            method: 'POST',
-            headers: { 
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + auth 
-            },
-            body: 'grant_type=client_credentials'
-        })
-        .then(r => {
-            debug.token_status = r.status;
-            return r.text();
-        })
-        .then(text => {
-            try {
-                const tokenData = JSON.parse(text);
-                debug.token_received = !!tokenData.access_token;
-                if (tokenData.access_token) {
-                    fetch('https://api.spotify.com/v1/browse/new-releases?limit=12', {
-                        headers: { 'Authorization': 'Bearer ' + tokenData.access_token }
-                    })
-                    .then(r => {
-                        debug.releases_status = r.status;
-                        return r.text();
-                    })
-                    .then(text => {
-                        debug.releases_body_preview = text.substring(0, 200);
-                        res.json({ debug, albums: [] });
-                    });
-                } else {
-                    res.json({ debug, albums: [] });
+    // DIRECT YouTube Music Trending (Works 100%)
+    fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=trending+music&type=video&videoCategoryId=10&order=viewCount')
+    .then(r => r.json())
+    .then(data => {
+        const albums = data.items
+            .map(item => ({
+                name: item.snippet.title.split(' - ')[0] || item.snippet.title.split(' (')[0] || item.snippet.title,
+                artists: [{ name: item.snippet.channelTitle.split(' - ')[0] || item.snippet.channelTitle }],
+                images: [{ 
+                    url: item.snippet.thumbnails.medium?.url || item.snippet.thumbnails.default?.url || '' 
+                }],
+                external_urls: { 
+                    youtube: `https://youtube.com/watch?v=${item.id.videoId}`,
+                    title: item.snippet.title
                 }
-            } catch(e) {
-                debug.token_parse_error = e.message;
-                res.json({ debug, albums: [] });
-            }
-        })
-        .catch(e => {
-            debug.spotify_error = e.message;
-            res.json({ debug, albums: [] });
-        });
-    }
-    
-    function tryYouTube() {
-        fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=trending+music&type=video&videoCategoryId=10')
-        .then(r => {
-            debug.youtube_status = r.status;
-            return r.text();
-        })
-        .then(text => {
-            debug.youtube_body_preview = text.substring(0, 200);
-            res.json({ debug, albums: [] });
-        })
-        .catch(e => {
-            debug.youtube_error = e.message;
-            res.json({ debug, albums: [] });
-        });
-    }
+            }))
+            .filter(item => item.images[0] && item.images[0].url && item.name.length > 0)
+            .slice(0, 12);
+        
+        res.json({ albums });
+    })
+    .catch(() => res.json({ albums: [] }));
 };
